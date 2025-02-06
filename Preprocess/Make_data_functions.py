9cf860a41a07bfdc964cec508d0e527117b920ff
rlbaiman <rebecca.baiman@colorado.edu>
2025-02-06 15:35:26 -0700

    Preprocess folder

---
+++ Preprocess/Make_data_functions.py
@@ -0,0 +1,23 @@
+def get_variable_names():
+    return([
+    'H500_lead0', 
+    'U800_lead0',
+    'V800_lead0',
+    'SLP_lead0',
+    'IWV_lead0'])
+def get_variables():
+    return([
+    'H',
+    'U',
+    'V',
+    'SLP',
+    'IWV'])
+
+def get_variable_level():
+    return([
+    '500',
+    '800',    
+    '800',    
+    None,  
+    None])
+
